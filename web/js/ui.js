c3873d2832a5c25072364cf4052ee62e44e5fd7e
Anoop S <anoop.santhanam@gmail.com>
2017-12-15 00:09:59 +0530

    Added error handling

--- web/js/ui.js
+++ web/js/ui.js
@@ -82,6 +82,20 @@ function validate(str){
 		return false;
 	}
 }
+var getUrlParameter = function getUrlParameter(sParam) {
+	var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+			sURLVariables = sPageURL.split('&'),
+			sParameterName,
+			i;
+
+	for (i = 0; i < sURLVariables.length; i++) {
+			sParameterName = sURLVariables[i].split('=');
+
+			if (sParameterName[0] === sParam) {
+					return sParameterName[1] === undefined ? true : sParameterName[1];
+			}
+	}
+};
 function readParams(){
 	var err=getUrlParameter('err');
 	if(validate(err)){
