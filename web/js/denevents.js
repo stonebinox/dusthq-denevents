9624d5db942612731e621f14e8444c7ffe817d7e
Anoop S <anoop.santhanam@gmail.com>
2017-12-17 22:29:03 +0530

    Added user dashboard

--- web/js/denevents.js
+++ web/js/denevents.js
@@ -251,7 +251,7 @@ app.controller("profile",function($scope,$compile,$http){
             $("#accountheader").addClass("dropdown-toggle");
             $("#accountheader").attr("data-toggle","dropdown");
             $("#accountheader").parent().attr("ng-init",'savedCount=0;ticketCount=0;');
-            $("#accountheader").parent().append('<ul class="dropdown-menu"><li><a href="#">Tickets <span class="badge">{{ticketCount}}</span></a></li><li><a href="#">Saved <span class="badge">{{savedCount}}</span></a></li><li><a href="dashboard">Manage events</a></li><li><a href="#">Contacts</a></li><li class="active"><a href="profile">Account settings</a></li><li><a href="logout">Log out</a></li></ul>');
+            $("#accountheader").parent().append('<ul class="dropdown-menu"><li><a href="#">Tickets <span class="badge">{[{ticketCount}]}</span></a></li><li><a href="#">Saved <span class="badge">{[{savedCount}]}</span></a></li><li><a href="dashboard">Manage events</a></li><li><a href="#">Contacts</a></li><li class="active"><a href="profile">Account settings</a></li><li><a href="logout">Log out</a></li></ul>');
             $compile($("#accountheader").parent())($scope);
         }
     }
@@ -469,7 +469,7 @@ app.controller("dashboard",function($scope,$compile,$http){
             $("#accountheader").addClass("dropdown-toggle");
             $("#accountheader").attr("data-toggle","dropdown");
             $("#accountheader").parent().attr("ng-init",'savedCount=0;ticketCount=0;');
-            $("#accountheader").parent().append('<ul class="dropdown-menu"><li><a href="#">Tickets <span class="badge">{{ticketCount}}</span></a></li><li><a href="#">Saved <span class="badge">{{savedCount}}</span></a></li><li><a href="dashboard">Manage events</a></li><li><a href="#">Contacts</a></li><li class="active"><a href="profile">Account settings</a></li><li><a href="logout">Log out</a></li></ul>');
+            $("#accountheader").parent().append('<ul class="dropdown-menu"><li><a href="#">Tickets <span class="badge">{[{ticketCount}]}</span></a></li><li><a href="#">Saved <span class="badge">{[{savedCount}]}</span></a></li><li><a href="dashboard">Manage events</a></li><li><a href="#">Contacts</a></li><li class="active"><a href="profile">Account settings</a></li><li><a href="logout">Log out</a></li></ul>');
             $compile($("#accountheader").parent())($scope);
         }
     }
