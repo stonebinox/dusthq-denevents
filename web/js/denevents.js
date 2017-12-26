1aae44d1202c7ce2aaa4100964586ca9698642e0
Anoop S <anoop.santhanam@gmail.com>
2017-12-27 00:38:30 +0530

    Displaying events

--- web/js/denevents.js
+++ web/js/denevents.js
@@ -63,14 +63,12 @@ app.controller("home", function($scope, $compile, $http) {
                 var eventAddress=stripslashes(nl2br(event.event_address));
                 // text += '<div class="col-md-4"><div class="thumbnail"><a href="event/' + eventID + '"><img src="' + eventImage + '" alt="' + eventName + '" class="img-responsive" style="width:250px;"><div class="caption"><p class="text-center"><strong>' + eventName + '</strong><br><span class="text-info">' + typeName + '</span></p></div></a></div></div>';
                 text += '<div class="col-12 col-md-6 col-lg-6 col-xl-4 wow fadeInUp mb-4">'
-                +'<div class="card lis-brd-light text-center text-lg-left"><a href="#"><div class="lis-grediant grediant-tb-light2 lis-relative modImage rounded-top"><img src="'+eventImage+'" alt="'+eventName+'" class="img-fluid rounded-top w-100" /> </div>'+
-                '<div class="lis-absolute lis-right-20 lis-top-20">'+
-                    '<div class="lis-post-meta border border-white text-white rounded lis-f-14">';
+                +'<div class="card lis-brd-light text-center text-lg-left"><a href="#"><div class="lis-grediant grediant-tb-light2 lis-relative modImage rounded-top"><img src="'+eventImage+'" alt="'+eventName+'" class="img-fluid rounded-top w-100" /> </div><div class="lis-absolute lis-right-20 lis-top-20"><div class="lis-post-meta border border-white text-white rounded lis-f-14">';
                 if(stat==2){
                     text+='Coming soon';
                 }
                 else{
-                    text='Open';
+                    text+='Open';
                 }
                 text+='</div></div></a><div class="card-body pt-0"><div class="media d-block d-lg-flex lis-relative"> <img src="'+userDP+'" alt="'+userName+'" class="lis-mt-minus-15 img-fluid d-lg-flex mx-auto mr-lg-3 mb-4 mb-lg-0 border lis-border-width-2 rounded-circle border-white" width="80" /><div class="media-body align-self-start mt-2"><h6 class="mb-0 lis-font-weight-600"><a href="#" class="lis-dark">'+eventName+'</a></h6></div></div><ul class="list-unstyled my-4 lis-line-height-2"><li><i class="fa fa-user-o pr-2"></i> '+orgName+'</li><li><i class="fa fa-map-o pr-2"></i> '+eventAddress+'</li></ul><div class="clearfix"><div class="float-none float-lg-left mb-3 mb-lg-0 mt-1"><A href="#" class="text-white"><i class="icofont icofont-travelling px-2 lis-bg5 py-2 lis-rounded-circle-50 lis-f-14"></i></A><A href="#" class="lis-id-info lis-light p-2 lis-rounded-circle-50 lis-f-14">1 More...</A> </div><div class="float-none float-lg-right mt-1"><A href="#" class="lis-light lis-f-14"><i class="fa fa-envelope-o lis-id-info  lis-rounded-circle-50 text-center"></i></A><A href="#" class="lis-light lis-f-14"><i class="fa fa-heart-o lis-id-info  lis-rounded-circle-50 text-center"></i></A><A href="#" class="lis-green-light text-white p-2 lis-rounded-circle-50 lis-f-14"><i class="fa fa-star"></i> 4.0</A> </div></div></div></div></div>';
             }
